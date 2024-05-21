ad5496baea5f2e9a68156229fe5abf7d0e706004
Michael Carenzo <79934822+mikecarenzo@users.noreply.github.com>
2024-05-21 14:09:33 -0400

    update results dictionary tests

--- tests/calculator/test_calculate_results.py
+++ tests/calculator/test_calculate_results.py
@@ -4,21 +4,21 @@ import logging
 def test_calculate_results(example_calculator):
 
     # Get default results dictionary.
-    assert example_calculator.calculate_results(125).get("Projected CVSS") == 7.5
+    assert example_calculator.calculate_results(125).get("projected_cvss") == 7.5
 
 
 def test_calculate_results_verbose(example_calculator):
 
     # Set calculator verbose mode to get wider results.
     assert example_calculator.calculate_results(125) == {
-        "Projected CVSS": 7.5,
-        "CWE": 125,
-        "Count": 1,
-        "Min CVSS Base Score": 7.5,
-        "Max CVSS Base Score": 7.5,
-        "Average CVSS Base Score": 7.5,
-        "Standard Deviation CVSS Base Score": 0.0,
-        "CVE Records": ["CVE-2014-0160"],
+        "projected_cvss": 7.5,
+        "cwe": 125,
+        "count": 1,
+        "min_cvss_base_score": 7.5,
+        "max_cvss_base_score": 7.5,
+        "avg_cvss_base_score": 7.5,
+        "std_dev_cvss_base_score": 0.0,
+        "cve_records": ["CVE-2014-0160"],
     }
 
 
